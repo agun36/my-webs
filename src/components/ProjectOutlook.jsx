305a7294fb80556d38d4b07c3855a971eb1c763e
Agun Akindele <aguntoroti10@gmail.com>
2023-04-02 19:16:34 +0100

    card styling

--- src/components/ProjectOutlook.jsx
+++ src/components/ProjectOutlook.jsx
@@ -1,40 +1,29 @@
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 import { Data } from './Data'
+import { Link } from 'react-router-dom'
 const ProjectOutlook = () => {
   return (
     <Container id='project'>
       <h2 className='text-center  mb-5'>
         Frontend Mentor <br /> Project Outlook
       </h2>
-      <Row
-        className=' g-4 pb-4 justify-content-center  '
-        style={{
-          '@media (min-width:992px)': {
-            // width: '50%',
-          },
-        }}
-      >
+      <Row className=' g-4 justify-content-center m-5 p-5 '>
         {Data.map((item) => (
-          <Col
-            className=''
-            sm={6}
-            md={4}
-            style={{
-              '@media (min-width:767px)': {
-                minWidth: '1rem',
-              },
-            }}
-          >
-            <Card key={item.id}>
+          <Col xs={12} md={6} style={{ maxWidth: '20rem' }}>
+            <Card key={item.id} className=''>
               <Card.Img src={item.project}></Card.Img>
               <Card.Body>
-                <Card.Title>
-                  <h3 className=''>{item.title}</h3>
-                </Card.Title>
-                <Card.Text>
-                  <p className=''> {item.text}</p>
-                </Card.Text>
-                <Button>{item.link}</Button>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>{item.text}</Card.Text>
+                <Link to={item.link}>
+                  <Button
+                    onClick={() => {
+                      window.location.href = item.link
+                    }}
+                  >
+                    Find More
+                  </Button>
+                </Link>
               </Card.Body>
             </Card>
           </Col>
